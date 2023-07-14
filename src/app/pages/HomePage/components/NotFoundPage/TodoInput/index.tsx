ad4f771c5dec851e2a6831e14cc1682e42ae6e5c
SSangbong <pch1286@gmail.com>
2023-07-15 00:35:29 +0900

    4chapter done

---
+++ src/app/pages/HomePage/components/NotFoundPage/TodoInput/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%auto;
+  font-size: 1.1em;
+  border-bottom: 1px solid #eee;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  outline: 0;
+`;
+
+export default function TodoInput({
+  setTodoList,
+}: {
+  setTodoList: (todo: ITodoItem) => void;
+}) {
+  const [content, setContent] = React.useState<string>('');
+  return (
+    <Box>
+      <Input
+        placeholder="할일을 입력해 주세요"
+        value={content}
+        onChange={e => setContent(e.target.value)}
+        onKeyPress={e => {
+          if (content === '') return;
+          if (e.key !== 'Enter' && e.key !== 'NumpadEnter') return;
+          setTodoList({
+            id: '0',
+            content: content,
+            completed: false,
+            editing: false,
+          });
+          setContent('');
+        }}
+      />
+    </Box>
+  );
+}
